819c7daf778cf59b93e3656b2033d0308a5bc65e
Adi Kurniawan <adikurniawan.dev@gmail.com>
2023-01-31 18:44:46 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const { AuthRouters } = require("./api/v1/routes");
+const { errorHandler } = require("./api/v1/middlewares");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(cors());
+
+const API_VERSION = "v1";
+
+app.use(`/${API_VERSION}/auth`, AuthRouters);
+app.use(errorHandler);
+
+module.exports = app;
